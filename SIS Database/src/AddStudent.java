8e028c9e9df39b3a1a19b2ffaf9d5a456286a415
Classified56 <Classified56@DavisRanney.PK5001Z>
2015-10-19 19:54:51 -0600

    Finished, everything works

--- SIS Database/src/AddStudent.java
+++ SIS Database/src/AddStudent.java
@@ -1,14 +1,14 @@
 import java.util.ArrayList;
-import java.io.IOException;
-import java.util.*;
+import java.util.Scanner;
 
 public class AddStudent 
-
-
 {
- 
 	public static void addStudentRun()
 	{
+		ArrayList<String> classes = new ArrayList<String>();
+		classes.add("Biology");
+		classes.add("English");
+		classes.add("Algebra");
 		Scanner userInput = new Scanner(System.in);
 		
 		System.out.println("What is the first name of the student");
@@ -16,77 +16,63 @@ public class AddStudent
 		System.out.println("What is the last name of the student");
 		String ln = userInput.nextLine();
 		System.out.println("What is the students first period?");
-		System.out.println("1) Biology");
-		System.out.println("2) Algebra");
-		System.out.println("3) English");
-		
+		for(int i = 0; i < classes.size(); i++)
+			{
+				System.out.println((i + 1) + ") " + classes.get(i));
+			}
 		int p1Choice = userInput.nextInt();
 		String p1Class = "";
-		
 		switch(p1Choice)
 		{
-		case 1: p1Class = "Biology"; 
+		case 1: p1Class = classes.get(0); 
+		classes.remove(0);
 		break;
 		
-		case 2: p1Class = "Algebra";
+		case 2: p1Class = classes.get(1);
+		classes.remove(1);
 		break;
 		
-		case 3: p1Class = "English";
+		case 3: p1Class = classes.get(2);
+		classes.remove(2);
 		break;
 		}	
 		
 		System.out.println("What is the students grade in "+p1Class);
-		String p1Grade = userInput.next();
+		String p1Grade = userInput.next().toUpperCase();
 		
 		System.out.println("What is the students second period?");
-		System.out.println("1) Biology");
-		System.out.println("2) Algebra");
-		System.out.println("3) English");
+		for(int i = 0; i < classes.size(); i++)
+			{
+				System.out.println((i + 1) + ") " + classes.get(i));
+			}
 		int p2Choice = userInput.nextInt();
 	
 		String p2Class = "";
 		
 		switch(p2Choice)
 		{
-		case 1: p2Class = "Biology"; 
+		case 1: p2Class = classes.get(0); 
+		classes.remove(0);
 		break;
 		
-		case 2: p2Class = "Algebra";
-		break;
-		
-		case 3: p2Class = "English";
+		case 2: p2Class = classes.get(1);
+		classes.remove(1);
 		break;
 		}
 		
 		System.out.println("What is the students grade in "+p2Class);
-		String p2Grade = userInput.next();
+		String p2Grade = userInput.next().toUpperCase();
 		
-		System.out.println("What is the students third period?");
-		System.out.println("1) Biology");
-		System.out.println("2) Algebra");
-		System.out.println("3) English");
-		int p3Choice = userInput.nextInt();
-		String p3Class = "";
-		
-		switch(p3Choice)
-		{
-		case 1: p3Class = "Biology"; 
-		break;
-		
-		case 2: p3Class = "Algebra";
-		break;
-		
-		case 3: p3Class = "English";
-		break;
-		}	
-		
+		String p3Class = classes.get(0);
+		System.out.println("Third period: "+p3Class);
 		System.out.println("What is the students grade in "+p3Class);
-		String p3Grade = userInput.next();
+		String p3Grade = userInput.next().toUpperCase();
 		
 		importFile.studentList.add(new Student(fn, ln,p1Class,p1Grade, p2Class, p2Grade,p3Class,p3Grade));
 		
-		System.out.println( fn + ln + " has been added");
-		System.out.println("Welcome to class");
+		SortByGPA.calculateGPA(importFile.studentList.size()-1);
+		SortByGPA.printStudentsInfo();
+		System.out.println();
 	}
 	
 }
